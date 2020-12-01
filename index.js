c3a1b45b775e8be01ae6d829856cff523545cce2
victor619 <victor619@github.com>
2020-12-01 14:57:28 +0100

    An application that stores the link to your faavourite book

---
+++ index.js
@@ -0,0 +1,80 @@
+// variables
+const book = document.getElementById('book')
+const url = document.getElementById('url')
+const button = document.getElementById('btn')
+const output = document.getElementById('list')
+const body = document.getElementsByTagName('body')
+// const regEx = 
+let list = []
+let index = 0
+// =======================================================
+
+button.addEventListener('click', addToList)
+
+
+// =======================================================
+
+// store books
+function addToList(){
+let library ={
+    name:book.value,
+    url:url.value
+}
+
+let string = list.push(library)
+localStorage.setItem('books',JSON.stringify(list))
+
+display()
+}
+
+function display(){
+    let books = localStorage.getItem('books')
+    let data = JSON.parse(books)
+    
+         let ourHTML = `
+         <div class='book'> 
+         <p><a href =${url.value}>${book.value}</a></p>
+         <button id = 'del' onclick='expel(${url.value}'>delete</button>
+
+         </div>
+         `
+         output.insertAdjacentHTML('beforeend',ourHTML)
+   
+}
+
+function getBooks(){
+    let books = localStorage.getItem('books')
+    let data = JSON.parse(books)
+
+    for (let i = 0; i < data.length; i++) {
+        let ourHTML = `
+        <div class='book'> 
+        <p><a href =${data[i].url}>${data[i].name}</a></p>
+        <button id = 'del' onclick='expel(${url.value})'>delete</button>
+        </div>
+        `
+        output.insertAdjacentHTML('beforeend',ourHTML)
+        
+    }
+  
+}
+
+function expel(url,element){
+
+    let books = localStorage.getItem('books')
+    let data = JSON.parse(books)
+     for (let i = 0; i < data.length; i++) {
+          if(books[i].url == url)
+          data.splice(i,1)
+console.log(data)
+         }   
+    localStorage.setItem('books',JSON.stringify(data))
+  
+     removeFromScreen()
+}
+
+function removeFromScreen(){
+    let del = document.getElementById('del')
+    del.parentElement.remove()
+
+}
